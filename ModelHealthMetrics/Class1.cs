c21c6e1bf9f3c9da46691fa0c7baf7ee5fb186f1
ChristopherVoltl <christopher.voltl@gmail.com>
2024-10-13 20:38:56 -0400

    Add ModelHealthMetrics class and Power BI integration
    Introduced the `ModelHealthMetrics` class implementing `IExternalCommand`. Added `CreateJsonPayload` method to generate JSON payloads from model health metrics. Integrated JSON payload creation into the `Execute` method of `ModelHealthMetrics` and added functionality to send this data to Power BI using the new `PowerBIPush` class. Display a `TaskDialog` to confirm successful data transmission. Removed the `CreateJsonPayload` method from its previous location.

--- ModelHealthMetrics/Class1.cs
+++ ModelHealthMetrics/Class1.cs
@@ -7,13 +7,26 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.Attributes;
+using Newtonsoft.Json;
 
 
 namespace Modelhealth
 {
     [Transaction(TransactionMode.Manual)]
+
     public class ModelHealthMetrics : IExternalCommand
     {
+        public string CreateJsonPayload(int totalElements, int warningCount, Dictionary<string, int> familyInstances)
+        {
+            var json = new
+            {
+                TotalElements = totalElements,
+                Warnings = warningCount,
+                Families = familyInstances
+            };
+
+            return JsonConvert.SerializeObject(json);
+        }
         // Implement the Execute method of the IExternalCommand interface
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -25,11 +38,18 @@ namespace Modelhealth
             int warningCount = GetWarningCount(doc);
             Dictionary<string, int> familyInstances = GetFamilyInstanceCounts(doc);
 
-            // (Optional) Show data in a TaskDialog or do additional processing
+            //Show data in a TaskDialog or do additional processing
             TaskDialog.Show("Revit Model Health",
                 $"Total Elements: {totalElements}\nWarnings: {warningCount}");
 
-            // You can also push the data to PowerBI here if needed
+            // Prepare the JSON payload
+            string jsonPayload = CreateJsonPayload(totalElements, warningCount, familyInstances);
+
+            // Send the payload to Power BI
+            PowerBIPush powerBIPush = new PowerBIPush();
+            Task.Run(() => powerBIPush.PushDataToPowerBI(jsonPayload)).Wait();
+
+            TaskDialog.Show("Revit Model Health", "Data has been successfully sent to Power BI.");
 
             return Result.Succeeded;
         }
@@ -62,8 +82,10 @@ namespace Modelhealth
         }
     }
 
+    
     public class PowerBIPush
-    {
+    {   
+        // PowerBI API URL
         private static readonly string apiUrl = "https://api.powerbi.com/beta/e66e77b4-5724-44d7-8721-06df160450ce/datasets/784e3d0c-eda4-431a-bf73-c49de06cf829/rows?experience=power-bi&key=Sjy3QOn2VB7ZbDXUJYRUEfhEC3vwTnB9VDHMRcn4p8IViv%2B2eE73IBRfzBUDmYvyiLqPNe%2FayWra%2FeYQ1hKHyg%3D%3D";
 
         public async Task PushDataToPowerBI(string jsonPayload)
@@ -79,18 +101,6 @@ namespace Modelhealth
                     throw new Exception("Failed to post data to PowerBI");
                 }
             }
-        }
-
-        public string CreateJsonPayload(int totalElements, int warningCount, Dictionary<string, int> familyInstances)
-        {
-            var json = new
-            {
-                TotalElements = totalElements,
-                Warnings = warningCount,
-                Families = familyInstances
-            };
-
-            return Newtonsoft.Json.JsonConvert.SerializeObject(json);
         }
     }
 }
